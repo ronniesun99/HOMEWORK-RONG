a8ab8494d985854f4b3958abdd4bd72461fff9bb
ronniesun99 <90742146+ronniesun99@users.noreply.github.com>
2021-09-22 15:59:32 -0400

    Add files via upload

---
+++ BankAccount.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace assignment_day2
+{
+    class BankAccount
+    {
+        public int UserId { get; set; }
+        public string CustomerName { get; set; }
+        public string MobileNumber { get; set; }
+        public string Email { get; set; }
+        public string Address { get; set; }
+        protected void CreateAccount()
+        {
+            Console.Write("Enter UserID = ");
+            UserId = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter customer name = ");
+            CustomerName = Console.ReadLine();
+            Console.Write("Enter mobilphone number = ");
+            MobileNumber = Console.ReadLine();
+            Console.Write("Enter Email = ");
+            Email = Console.ReadLine();
+            Console.Write("Enter address = ");
+            Address = Console.ReadLine();
+        }
+        protected void PrintAccount()
+        {
+            Console.WriteLine("User ID ="+UserId);
+            Console.WriteLine("Customer name ="+CustomerName);
+            Console.WriteLine("Mobilphone number ="+MobileNumber);
+            Console.WriteLine("Email = "+Email);
+            Console.WriteLine("Address ="+Address);
+        }
+    }
+    class CheckingAccount:BankAccount
+    {
+        public string AccountNumber { get; set; }
+        public decimal AvailableBalance { get; set; }
+        public void AddCheckingAccount()
+        {
+            CreateAccount();
+            Console.Write("Enter checking account number=");
+            AccountNumber = Console.ReadLine();
+            Console.Write("Enter Available balance =");
+            AvailableBalance = Convert.ToDecimal(Console.ReadLine());
+        }
+        public void PrintCheckingAccount()
+        {
+            PrintAccount();
+            Console.WriteLine("Checking account number ="+AccountNumber);
+            Console.WriteLine("Available balance ="+AvailableBalance);
+        }
+    }
+    class SavingAccount:BankAccount
+    {
+        public string AccountNumber { get; set; }
+        public decimal AvailableBalance { get; set; }
+        public void AddSavingAccount()
+        {
+            CreateAccount();
+            Console.Write("Enter saving account number=");
+            AccountNumber = Console.ReadLine();
+            Console.Write("Enter Available balance =");
+            AvailableBalance = Convert.ToDecimal(Console.ReadLine());
+        }
+        public void PrintSavingAccount()
+        {
+            PrintAccount();
+            Console.WriteLine("Saving account number =" + AccountNumber);
+            Console.WriteLine("Available balance =" + AvailableBalance);
+        }
+    }
+}
+
---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace assignment_day2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //SavingAccount sa = new SavingAccount();
+            //sa.AddSavingAccount();
+            //sa.PrintSavingAccount();
+
+            CheckingAccount ca = new CheckingAccount();
+            ca.AddCheckingAccount();
+            ca.PrintCheckingAccount();
+        }
+    }
+}
